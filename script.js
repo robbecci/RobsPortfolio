e555d2a5ce8d86b174bed02e5dc063e0d6defa1c
robbecci <dinapolirobertofelice@gmail.com>
2025-11-25 18:27:37 +0100

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+// Testo dinamico nella Home
+const dynamicText = document.getElementById("dynamic-text");
+const words = ["Programmatore", "Unity", "C++", "C#"];
+let index = 0;
+
+setInterval(() => {
+    index = (index + 1) % words.length;
+    dynamicText.textContent = words[index];
+}, 3000);
+
+// Particelle animate
+const canvas = document.getElementById("bg-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+const numParticles = 100;
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    }
+    draw() {
+        ctx.fillStyle = "#ff6600";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function initParticles() {
+    particles = [];
+    for (let i = 0; i < numParticles; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas
